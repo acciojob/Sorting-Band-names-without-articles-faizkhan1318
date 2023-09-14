a6a2adb24b329322e5a8f8ff16342985d0f1f2b6
Faisel Khan <125124484+faizkhan1318@users.noreply.github.com>
2023-09-14 15:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+  let regexp = /\bthe\b|\ban\b|\ba\b/gi;
 
+  let articleLessArray = [];
+  let mp = {};
+
+  for(let i=0; i<touristSpots.length; i++){
+    //trim is used for remove the space from front side
+    let articleLessItem = touristSpots[i].replace(regexp, "").trim();
+    articleLessArray.push(articleLessItem);
+
+    mp[articleLessItem] = touristSpots[i];
+  }
+
+  articleLessArray.sort();
+  let anss = [];
+
+  for(let i of articleLessArray){
+    anss.push(mp[i]);
+  }
+  console.log(anss);
